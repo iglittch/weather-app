79c00f0c04d741bb1f92bbf21eb3d3369354338f
Sophia Jama <72500901+iglittch@users.noreply.github.com>
2023-12-24 05:57:42 +0300

    Updated search engine

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
+function updateInterface(response) {
+  let temperature = document.querySelector("#temperature");
+  temperature.innerHTML = Math.round(response.data.temperature.current);
+  let city = document.querySelector("#city");
+  let country = document.querySelector("#country");
+  country.innerHTML = response.data.country;
+  city.innerHTML = response.data.city;
+}
+function citySearch(city) {
+  let apiKey = "9984bb0t812a11f3bb79o019ab7e5b3f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=$${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updateInterface);
+}
+
 function updateCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let city = document.querySelector("#city");
-  city.innerHTML = searchInput.value;
+
+  citySearch(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
